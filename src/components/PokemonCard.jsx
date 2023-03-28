e51e5132101fcd32c62153db6536bd117f553b90
Pujol <pujol.maxime31@gmail.com>
2023-03-28 12:43:04 +0200

    components1

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+function PokemonCard() {
+    return(
+        <div><figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"  />
+
+        </figure>
+        <figcaption>bulbasaur</figcaption>
+
+        </div>
+
+    )
+
+    
+}
+export default PokemonCard
